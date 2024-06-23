1e5b2c3a98516990cfd10e1d55205348730d1995
Charalampos Kontos <har.kontos@hotmail.com>
2024-06-23 21:30:58 +0300

    Added Screenshot

---
+++ CosmicViewMvc/Controllers/NasaPictureController.cs
@@ -0,0 +1,17 @@
+﻿using CosmicViewSharedLib.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CosmicViewMvc.Controllers
+{
+    public class NasaPicturesController : Controller
+    {
+        private readonly IPictureService _pictureService;
+
+        public NasaPicturesController(IPictureService pictureService)
+        {
+            _pictureService = pictureService;
+        }
+
+
+    }
+}
